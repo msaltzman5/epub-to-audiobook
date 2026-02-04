f7af6457dd18d09ee57cfe934c12623530f9fdc5
msaltzman5 <mclippers2002@gmail.com>
2026-02-03 22:49:17 -0600

    adding pdf to text

--- README.md
+++ README.md
@@ -11,3 +11,12 @@
 
 ## Usage
 `edge-tts --voice en-US-AndrewNeural --text "Hi! How are you?" --write-media test.mp3`
+
+## pdf to text
+1. Download Xpdf command line required  
+2. Unzip download so you can execute the binaries  
+3. /home/msaltzman/Downloads/xpdf-tools-linux-4.06/bin64/pdftotext <books_directory>/<book>.pdf  
+    a. pdftotext man: https://www.xpdfreader.com/pdftotext-man.html  
+4. `python3 main.py`  
+    a. this should clean txt file
+5. edge-tts --voice en-US-AndrewNeural --file <books_directory>/<book_cleaned.pdf> --write-media test.mp3
---
+++ main.py
@@ -0,0 +1,22 @@
+def main():
+
+    def remove_c1_control_characters(text):
+        """
+        Remove C1 control characters (0x80-0x9F) from a string.
+        These often appear due to incorrect encoding interpretation.
+        """
+        return ''.join(char for char in text if ord(char) not in range(0x80, 0xA0))
+
+    with open('books/zen.txt', 'r', encoding='latin-1') as f:
+        data = f.read()
+    
+
+    cleaned_data = remove_c1_control_characters(data)
+
+    with open('books/zen_cleaned.txt', 'w') as f:
+        f.write(cleaned_data)
+
+    return 0
+
+if __name__ == "__main__":
+    main()
